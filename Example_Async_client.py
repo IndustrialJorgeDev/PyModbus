4876b17e2a457cbefe1982dcee5570e3dd27345c
IndustrialJorgeDev <jorgesanchezsanguino9@gmail.com>
2024-05-17 12:42:49 +0200

    Update Example_Async_client.py

--- Example_Async_client.py
+++ Example_Async_client.py
@@ -1,21 +1,27 @@
-import asyncio
+"""
+Este código configura un cliente asíncrono para conectarse a un servidor Modbus usando diferentes protocolos de comunicación (TCP, UDP, serial, TLS). 
+Realiza una lectura de coils y maneja posibles excepciones de Modbus, cerrando la conexión adecuadamente después de la operación.
 
-import pymodbus.client as ModbusClient
+"""
 
-from pymodbus import (
+import asyncio  # Importa la biblioteca asyncio para la programación asíncrona
+
+import pymodbus.client as ModbusClient  # Importa la clase ModbusClient del módulo pymodbus
+
+from pymodbus import (  # Importa varias excepciones y configuraciones del módulo pymodbus
     ExceptionResponse,
     Framer,
     ModbusException,
     pymodbus_apply_logging_config,
 )
 
-
 async def run_async_simple_client(comm, host, port, framer=Framer.SOCKET):
-    """Run async client."""
-    # activate debugging
+    """Ejecuta un cliente asíncrono."""
+    # Activa la depuración
     pymodbus_apply_logging_config("DEBUG")
 
-    print("get client")
+    print("Obtener cliente")
+    # Selecciona el tipo de cliente Modbus según el protocolo de comunicación (comm)
     if comm == "tcp":
         client = ModbusClient.AsyncModbusTcpClient(
             host,
@@ -65,36 +71,38 @@ async def run_async_simple_client(comm, host, port, framer=Framer.SOCKET):
             server_hostname="localhost",
         )
     else:
-        print(f"Unknown client {comm} selected")
+        print(f"Cliente desconocido {comm} seleccionado")
         return
 
-    print("connect to server")
-    await client.connect()
-    # test client is connected
+    print("Conectar al servidor")
+    await client.connect()  # Conecta al servidor de forma asíncrona
+    # Verifica si el cliente está conectado
     assert client.connected
 
-    print("get and verify data")
+    print("Obtener y verificar datos")
     try:
-        # See all calls in client_calls.py
-        rr = await client.read_coils(1, 1, slave=1)
+        # Realiza una lectura de los coils (bobinas) Modbus
+        rr = await client.read_holding_registers(1, 1, slave=1)
     except ModbusException as exc:
-        print(f"Received ModbusException({exc}) from library")
+        print(f"Recibió ModbusException({exc}) de la biblioteca")
         client.close()
         return
+    # Verifica si hubo un error en la respuesta
     if rr.isError():
-        print(f"Received Modbus library error({rr})")
+        print(f"Recibió un error de la biblioteca Modbus({rr})")
         client.close()
         return
+    # Verifica si la respuesta es una excepción de Modbus
     if isinstance(rr, ExceptionResponse):
-        print(f"Received Modbus library exception ({rr})")
-        # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
+        print(f"Recibió una excepción de la biblioteca Modbus ({rr})")
+        # ESTO NO ES UNA EXCEPCIÓN DE PYTHON, sino un mensaje válido de Modbus
         client.close()
 
-    print("close connection")
-    client.close()
+    print("Cerrar conexión")
+    client.close()  # Cierra la conexión
 
-
+# Punto de entrada principal del script
 if __name__ == "__main__":
     asyncio.run(
-        run_async_simple_client("tcp", "127.0.0.1", 5020), debug=True
+        run_async_simple_client("tcp", "192.168.2.4", 502), debug=True
     )
